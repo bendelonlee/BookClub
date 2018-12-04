2802b4cf884957b5da8a8c986f20a91fdb0d70a3
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-04 08:08:16 -0700

    adds seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "./app/models/book"
+
+Book.create(title: 'Under The Dome', author: 'Stephen King', page_count: 500)
+Book.create(title: 'Under The Dome', author: 'Stephen King', page_count: 500)
+Book.create(title: 'Under The Dome', author: 'Stephen King', page_count: 500)
+Book.create(title: 'Under The Dome', author: 'Stephen King', page_count: 500)
+Book.create(title: 'Under The Dome', author: 'Stephen King', page_count: 500)
+Book.create(title: 'Under The Dome', author: 'Stephen King', page_count: 500)
