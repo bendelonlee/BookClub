58ac023dd3740dfa508e2177ec4440a5da0097e8
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-03 18:18:08 -0700

    adds resources for index,show,and destroy routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/", to: "books#index"
-  get "/books", to: "books#index"
+  resources :books, only: [:index, :show, :destroy]
 end
