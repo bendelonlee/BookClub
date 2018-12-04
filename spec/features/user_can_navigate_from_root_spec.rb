7b83826bd1c0bd881c1f3c056a61bc91d02accb1
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-03 17:12:29 -0700

    adds features/ and test for navigation

---
+++ spec/features/user_can_navigate_from_root_spec.rb
@@ -0,0 +1,23 @@
+describe 'Root page navigation' do
+  context 'as a user' do
+    it 'should have a nav with working links' do
+      visit "/"
+
+      within "#homepage" do
+        expect(page).to have_content("BookClub")
+      end
+
+      within "#books" do
+        expect(page).to have_content("All books")
+      end
+
+      click_link "books"
+
+      expect(current_path).to eq("/books")
+
+      click_link "homepage"
+
+      expect(current_path).to eq("/")
+    end
+  end
+end
