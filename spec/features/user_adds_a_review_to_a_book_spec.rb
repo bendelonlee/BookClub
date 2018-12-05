94a641a748f3796b960f57f31100f200e518c5e6
Ben Lee <bendelonlee@gmail.com>
2018-12-05 13:44:48 -0700

    Removes comment

--- spec/features/user_adds_a_review_to_a_book_spec.rb
+++ spec/features/user_adds_a_review_to_a_book_spec.rb
@@ -67,19 +67,3 @@ describe 'user_adds_a_review_to_a_book' do
 
   end
 end
-
-
-# As a Visitor,
-# When I visit a book's show page
-# I see a link to add a new review for this book.
-# When I click on this link, I am taken to a new review path.
-# On this new page, I see a form where I can enter:
-# - a review title
-# - my username as a string
-# - a numeric rating that can only be a number from 1 to 5
-# - some text for the review itself
-# When the form is submitted, I should return to that book's
-# show page and I should see my review text.
-#
-# User names should be converted to Title Case before saving.
-# User names should be unique within the system.
