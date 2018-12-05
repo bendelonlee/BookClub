30dc95d8be2d4f34e9ed7f97d2475655ed608c9a
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-05 13:50:12 -0700

    adds publish_year to book creation

--- spec/features/user_sees_one_book_spec.rb
+++ spec/features/user_sees_one_book_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 describe 'user sees the show page of one book' do
   it 'displays the books information and reviews' do
     author_1 = Author.create!(name: "Hemingway, Ernest")
-    book_1 = author_1.books.create!(title: "Long Day", page_count: 375)
+    book_1 = author_1.books.create!(title: "Long Day", page_count: 375, publish_year: 2012)
     user_1 = User.create!(name: "Bob")
     user_2 = User.create!(name: "Barb")
     review_1 = book_1.reviews.create!(title: "Wow.", rating: 5, user_id: user_1.id, text: "This book flew by! It was amazing!")
@@ -13,6 +13,7 @@ describe 'user sees the show page of one book' do
     expect(page).to have_content(book_1.title)
     expect(page).to have_content(author_1.name)
     expect(page).to have_content(book_1.page_count)
+    expect(page).to have_content(book_1.publish_year)
 
     expect(page).to have_content(user_1.name)
     expect(page).to have_content(review_1.title)
