4fcf5212d99680f3b6a37a150173362d1058f71f
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-05 19:04:07 -0700

    adds migration to remove rating from books

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,7 @@ class Review < ApplicationRecord
   validates_presence_of :title, :text
 
   validates :rating, presence: {message: "is a required field"}
-
-
+  
   belongs_to :user
   belongs_to :book
 end
