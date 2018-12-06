239f2714b4dae2fcd28a01552ab426b725532074
Ben Lee <bendelonlee@gmail.com>
2018-12-06 15:15:13 -0700

    Adds expects for second review in user feature test

--- spec/features/user_sees_one_user_spec.rb
+++ spec/features/user_sees_one_user_spec.rb
@@ -6,7 +6,7 @@ describe 'I click on a user name on a book review' do
     book_2 = author_1.books.create!(title: "Journey Of Dogs", page_count: 500, publish_year: 1900)
     @user_1 = User.create!(name: "Bob")
     @review_1 = book_1.reviews.create!(title: "Wow.", rating: 5, user_id: @user_1.id, text: "This book flew by! It was amazing!")
-    review_2 = book_2.reviews.create!(title: "OH.", rating: 1, user_id: @user_1.id, text: "I think I got this book after I read it and I was like .. Oh. ")
+    @review_2 = book_2.reviews.create!(title: "OH.", rating: 1, user_id: @user_1.id, text: "I think I got this book after I read it and I was like .. Oh.")
 
     visit book_path(book_1)
     click_on @user_1.name
@@ -22,10 +22,12 @@ describe 'I click on a user name on a book review' do
       expect(page).to have_content(@review_1.text)
     end
 
+    within "#review-#{@review_2.id}-container" do
+      expect(page).to have_content(@review_2.book.title)
+      expect(page).to have_content(@review_2.title)
+      expect(page).to have_content(@review_2.rating)
+      expect(page).to have_content(@review_2.text)
+    end
+
   end
 end
-# As a Visitor,
-# When I click on a user's name for any book review
-# I am taken to a show page for that user.
-# I should see all reviews (title, rating and text) that this
-# user has written.
