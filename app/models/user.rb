dac477a1b0371d28b7e4302ed39a663d9c69b991
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-07 14:06:44 -0700

    adds ordering by name, in addition to review count

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,11 @@ class User < ApplicationRecord
   validates_uniqueness_of :name
   has_many :reviews
 
-  def self.top_users_by_reviews(num_of_users)
+  def self.top_users_by_reviews(num_of_users = nil)
     joins(:reviews)
       .select("COUNT(user_id)")
       .group(:user_id, :name)
-      .order("COUNT(user_id) DESC")
+      .order("COUNT(user_id) DESC", :name)
       .limit(num_of_users)
       .pluck(:name, "COUNT(user_id)")
   end
