0f14c34c26e42b0aee6e9062874dacc9a1dffb5f
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-07 08:36:34 -0700

    adds shallow:true for books routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "books#index"
 
-  resources :books, only: [:index, :show, :destroy, :new, :create, :delete] do
-    resources :reviews, only: [:new, :create]
+  resources :books, only: [:index, :show, :destroy, :new, :create], shallow: true do
+    resources :reviews, only: [:new, :create, :destroy]
   end
-    
-  end
+
   resources :users, only: [:show]
   resources :authors, only: [:index, :show]
-
 end
