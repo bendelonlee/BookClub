3fe19cda23c56336cb3ebe0ee64e54670c55e5c3
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-07 13:35:04 -0700

    changed method name

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @authors = @book.authors
     @reviews = @book.reviews
-    @average_review = @book.average_review
+    @average_rating = @book.average_rating
     @top_reviews = @book.get_reviews(:top, 3)
     @bottom_reviews = @book.get_reviews(:bottom, 3)
     @bottom_reviews = @bottom_reviews.select {|rev| !@top_reviews.include?(rev)}
