d86a213e519f97e87deeaa60bb41a38ac1e42580
Ben Lee <bendelonlee@gmail.com>
2018-12-06 17:13:24 -0700

    Fixes form for bug for adding a book

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "books#index"
-  resources :books, only: [:index, :show, :destroy, :new, :create] do
+  resources :books, shallow: true, only: [:index, :show, :destroy, :new, :create] do
     resources :reviews, only: [:new, :create]
+
   end
-  resources :authors, only: [:index, :show]
   resources :users, only: [:show]
+  resources :authors, only: [:index, :show]
+
 end
