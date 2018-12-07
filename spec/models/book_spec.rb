cafe3703b5fe098abe19ed673a653a12d5a73107
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-07 13:30:24 -0700

    changes test review_average for book instances

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -10,22 +10,21 @@ RSpec.describe Book, type: :model do
     it { should have_many(:authors) }
     it { should have_many(:reviews) }
   end
+
   describe 'instance methods' do
     it "titleizes the title" do
       book_1 = Book.new(title: "hey you", page_count: 134)
       book_1.titleize_title
       expect(book_1.title).to eq("Hey You")
     end
+
     it '.review_average' do
-      book_1 = Book.new(title: "it's me", page_count: 134)
       user = User.create!(name: "Egress Mcgovern")
+      book_1 = Book.create!(title: "it's me", page_count: 134)
+      book_1.reviews.create!(title: "So lovely", rating: 5, text: "I love how they fell in love", user_id: user.id)
+      book_1.reviews.create!(title: "Too much", rating: 1, text: "This book was too much for me", user_id: user.id)
 
-      r_1 = Review.create!(title: "So lovely", rating: 5, text: "I love how they fell in love", user_id: user.id)
-      r_2 = Review.create!(title: "Too much", rating: 1, text: "This book was too much for me", user_id: user.id)
-
-      visit book_path(book_1.id)
-
-      expect(page).to have_content("Average Rating: #{book_1.reviews.average(:rating)}")
+      expect(book_1.review_average).to eq(3)
     end
   end
 end
