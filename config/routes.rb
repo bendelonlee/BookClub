1329d4ec9ae922ed30c824088226638c7c36eaa6
Ben Lee <bendelonlee@gmail.com>
2018-12-08 07:50:39 -0700

    Sets up welcome controller/page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "books#index"
+  root "welcome#homepage"
 
   resources :books, only: [:index, :show, :destroy, :new, :create], shallow: true do
     resources :reviews, only: [:new, :create, :destroy]
