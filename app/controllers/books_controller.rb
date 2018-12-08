25bca8a71564400201c45a3be8fc0a13eb09bf33
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-08 14:45:03 -0700

    changed average_rating name in books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @authors = @book.authors
     @reviews = @book.reviews
-    @average_rating = @book.average_rating
+    @average_review = @book.average_review
     @top_reviews = @book.get_reviews(:top, 3)
     @bottom_reviews = @book.get_reviews(:bottom, 3)
     @bottom_reviews = @bottom_reviews.select {|rev| !@top_reviews.include?(rev)}
