c9372512fdbb13b496d738f33406efa019cdc694
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-08 17:32:41 -0700

    adds initial testing for sorting user reviews

---
+++ spec/features/user_can_sort_reviews_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+describe 'User Reviews Sorting' do
+  before :each do
+    @user = User.create!(name: Faker::Name.unique.name)
+
+    @book = Book.create!(title: Faker::Book.unique.title, page_count: rand(900), publish_year: rand(1950..2018))
+    review_1 = @book.reviews.create!(title: "#{Faker::App.name} #{Faker::Ancient.god}", rating: rand(1..5), user_id: @user.id, text: Faker::RickAndMorty.quote)
+    review_2 = @book.reviews.create!(title: "#{Faker::App.name} #{Faker::Ancient.god}", rating: rand(1..5), user_id: @user.id, text: Faker::RickAndMorty.quote)
+    review_3 = @book.reviews.create!(title: "#{Faker::App.name} #{Faker::Ancient.god}", rating: rand(1..5), user_id: @user.id, text: Faker::RickAndMorty.quote)
+    review_4 = @book.reviews.create!(title: "#{Faker::App.name} #{Faker::Ancient.god}", rating: rand(1..5), user_id: @user.id, text: Faker::RickAndMorty.quote)
+    review_5 = @book.reviews.create!(title: "#{Faker::App.name} #{Faker::Ancient.god}", rating: rand(1..5), user_id: @user.id, text: Faker::RickAndMorty.quote)
+    review_6 = @book.reviews.create!(title: "#{Faker::App.name} #{Faker::Ancient.god}", rating: rand(1..5), user_id: @user.id, text: Faker::RickAndMorty.quote)
+
+    visit user_path(@user)
+  end
+
+  it 'should sort reviews by rating ascending and date ascending' do
+    
+
+  end
+  xit 'should sort reviews by rating ascending and date descending' do
+
+  end
+  xit 'should sort reviews by rating descending and date ascending' do
+
+  end
+  xit 'should sort reviews by rating descending and date descending' do
+
+  end
+end
