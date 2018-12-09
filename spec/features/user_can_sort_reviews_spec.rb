a34cfec752ce526874795de24120e332652a3b23
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-09 10:49:10 -0700

    adds testing that reviews appear in order

--- spec/features/user_can_sort_reviews_spec.rb
+++ spec/features/user_can_sort_reviews_spec.rb
@@ -21,15 +21,54 @@ describe 'User Reviews Sorting' do
 
     click_button "Sort"
 
-    expect(current_path).to eq(user_path(@user))
+    sorted = @user.reviews.order("rating asc, reviews.created_at asc")
+    expect(all('.review')[0]).to have_content(sorted[0].title, sorted[0].text)
+    expect(all('.review')[1]).to have_content(sorted[1].title, sorted[1].text)
+    expect(all('.review')[2]).to have_content(sorted[2].title, sorted[2].text)
+    expect(all('.review')[3]).to have_content(sorted[3].title, sorted[3].text)
+    expect(all('.review')[4]).to have_content(sorted[4].title, sorted[4].text)
+    expect(all('.review')[5]).to have_content(sorted[5].title, sorted[5].text)
   end
-  xit 'should sort reviews by rating ascending and date descending' do
+  it 'should sort reviews by rating ascending and date descending' do
+    page.select("Ascending", from: "By Rating")
+    page.select("Descending", from: "By Date")
 
+    click_button "Sort"
+
+    sorted = @user.reviews.order("rating asc, reviews.created_at desc")
+    expect(all('.review')[0]).to have_content(sorted[0].title, sorted[0].text)
+    expect(all('.review')[1]).to have_content(sorted[1].title, sorted[1].text)
+    expect(all('.review')[2]).to have_content(sorted[2].title, sorted[2].text)
+    expect(all('.review')[3]).to have_content(sorted[3].title, sorted[3].text)
+    expect(all('.review')[4]).to have_content(sorted[4].title, sorted[4].text)
+    expect(all('.review')[5]).to have_content(sorted[5].title, sorted[5].text)
   end
-  xit 'should sort reviews by rating descending and date ascending' do
+  it 'should sort reviews by rating descending and date ascending' do
+    page.select("Descending", from: "By Rating")
+    page.select("Ascending", from: "By Date")
 
+    click_button "Sort"
+
+    sorted = @user.reviews.order("rating desc, reviews.created_at asc")
+    expect(all('.review')[0]).to have_content(sorted[0].title, sorted[0].text)
+    expect(all('.review')[1]).to have_content(sorted[1].title, sorted[1].text)
+    expect(all('.review')[2]).to have_content(sorted[2].title, sorted[2].text)
+    expect(all('.review')[3]).to have_content(sorted[3].title, sorted[3].text)
+    expect(all('.review')[4]).to have_content(sorted[4].title, sorted[4].text)
+    expect(all('.review')[5]).to have_content(sorted[5].title, sorted[5].text)
   end
-  xit 'should sort reviews by rating descending and date descending' do
+  it 'should sort reviews by rating descending and date descending' do
+    page.select("Descending", from: "By Rating")
+    page.select("Descending", from: "By Date")
 
+    click_button "Sort"
+
+    sorted = @user.reviews.order("rating desc, reviews.created_at desc")
+    expect(all('.review')[0]).to have_content(sorted[0].title, sorted[0].text)
+    expect(all('.review')[1]).to have_content(sorted[1].title, sorted[1].text)
+    expect(all('.review')[2]).to have_content(sorted[2].title, sorted[2].text)
+    expect(all('.review')[3]).to have_content(sorted[3].title, sorted[3].text)
+    expect(all('.review')[4]).to have_content(sorted[4].title, sorted[4].text)
+    expect(all('.review')[5]).to have_content(sorted[5].title, sorted[5].text)
   end
 end
