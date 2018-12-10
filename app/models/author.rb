9fc63d07f5da805b394a73bee90d6b4ea3e72400
Ricardo Ledesma <ricardoledesmadev@gmail.com>
2018-12-10 10:54:38 -0700

    adds instance method to get all books review average for an author

--- app/models/author.rb
+++ app/models/author.rb
@@ -8,4 +8,10 @@ class Author < ApplicationRecord
   def titleize_name
     self.name = name.titleize if name
   end
+
+  def all_books_review_average
+    books.reduce(0) do |sum, book|
+      sum += book.reviews.average(:rating)
+    end/self.books.count
+  end
 end
